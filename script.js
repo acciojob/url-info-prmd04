20e6e7f1dbc174ea91ab5cc7a78a63eb5137b245
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-19 20:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL using the location object
+const currentUrl = location.href;
+
+
+const urlLength = currentUrl.length;
+
+
+alert("The length of the URL is: " + urlLength);
